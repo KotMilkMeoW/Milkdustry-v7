4deae574feb880e34726075ce193c24cc4f68b0c
KotMilkMeoW <81019712+KotMilkMeoW@users.noreply.github.com>
2021-06-30 14:12:11 +0300

    Рахмер снюса

--- core/src/mindustry/input/InputHandler.java
+++ core/src/mindustry/input/InputHandler.java
@@ -45,7 +45,7 @@ public abstract class InputHandler implements InputProcessor, GestureListener{
     final static float playerSelectRange = mobile ? 17f : 11f;
     final static IntSeq removed = new IntSeq();
     /** Maximum line length. */
-    final static int maxLength = 100;
+    final static int maxLength = 100000;
     final static Rect r1 = new Rect(), r2 = new Rect();
 
     public final OverlayFragment frag = new OverlayFragment();
